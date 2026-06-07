3df6deb27e212e435b7faa241842326d8722a9e5
Shayan Ghosh <158580294+Shayanghosh03@users.noreply.github.com>
2026-06-07 14:04:55 +0530

    DP ADD

---
+++ Dynamic Programming/Nth_Fibonacci_Number_Using_DP.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n, vector<int>& dp) {
+    // Base Case
+    if(n <= 1) {
+        return n;
+    }
+    // Step -> 3
+    if(dp[n] != -1) {
+        return dp[n];
+    }
+
+    // Step -> 2
+    dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // Step -> 1
+    vector<int> dp(n + 1, -1);
+    cout << fib(n, dp) << endl;
+
+    return 0;
+}
