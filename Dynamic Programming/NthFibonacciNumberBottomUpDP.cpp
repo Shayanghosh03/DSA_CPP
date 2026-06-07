f07d3bcdf5e1ee50de53058b46bfeda3c5199fec
Shayan Ghosh <158580294+Shayanghosh03@users.noreply.github.com>
2026-06-07 16:55:38 +0530

    Add SpaceOptimization Code

--- Dynamic Programming/NthFibonacciNumberBottomUpDP.cpp
+++ Dynamic Programming/NthFibonacciNumberBottomUpDP.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main()
 {
-        int n;
-        cin >> n;
+    int n;
+    cin >> n;
 
-        // Step -> 1
-        vector<int> dp(n+1);
+    // Step -> 1
+    vector<int> dp(n+1);
 
-        // Step -> 2
-        dp[0] = 0;
-        dp[1] = 1;
+    // Step -> 2
+    dp[0] = 0;
+    dp[1] = 1;
 
-        // Step => 3
-        for(int i = 2; i <= n; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-        }
+    // Step => 3
+    for(int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
 
-        cout<< dp[n] << endl;
+    cout<< dp[n] << endl;
 
-        return 0;
+    return 0;
 }
